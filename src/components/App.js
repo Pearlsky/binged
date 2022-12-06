9514aa9f7df117f83c636bca5d0f89f1bda7faac
Pearl Akpan <pearlakpan115@gmail.com>
2022-12-06 17:02:20 +0100

    refactor: add routes to test login and signup components

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,24 @@
-import '../App.scss';
-import Button from './Button';
+import {Routes, Route} from "react-router-dom";
+
+import GlobalStyles from '../utils/Global.styles';
+import * as ROUTES from "../constants/routes";
+
+import Login from "../views/Login/Login";
+import Signup from "../views/Signup/Signup";
 
 function App() {
   return (
     <>
-    <Button text="Sign Up"/>
+    <GlobalStyles/>
+
+    <Routes>
+      <Route path={ROUTES.SIGN_UP} element={<Signup/>}/>
+      <Route path={ROUTES.LOGIN} element={<Login/>}/>
+      {/* <Route path={ROUTES.HOME}/>
+      <Route path={ROUTES.MOVIES}/>
+      <Route path={ROUTES.TV_SERIES}/>
+      <Route path={ROUTES.BOOKMARKS}/> */}
+    </Routes>
     </>
   );
 }
