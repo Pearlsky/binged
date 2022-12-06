92bdde8e5146fab0bd4eb2569d7d83a9f78448c3
Pearl Akpan <pearlakpan115@gmail.com>
2022-12-06 16:52:35 +0100

    style: assign global util variables

---
+++ src/utils/variables.js
@@ -0,0 +1,13 @@
+export const colors = {
+  redprimary: `rgb(252, 71, 71)`,
+  darkblue: `rgb(16, 20, 30)`,
+  grayishblue: `rgb(90, 105, 143)`,
+  semidarkblue: `rgb(22, 29, 47)`,
+  white: `rgb(255, 255, 255)`,
+  purewhite: `rgb(255, 255, 255)`,
+};
+
+export const weights = {
+  light: 300,
+  medium: 500,
+};
