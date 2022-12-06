b126910e7509bbd6a94b6e7e555484e602a7aeaf
Pearl Akpan <pearlakpan115@gmail.com>
2022-12-06 17:13:26 +0100

    styles: write signup page UI

---
+++ src/views/Signup/Signup.js
@@ -0,0 +1,8 @@
+import AuthForm from "../../components/AuthForm/AuthForm";
+import StyledSignup from "./Signup.styles";
+
+const Signup = () => {
+    return <StyledSignup><AuthForm heading="sign up"/></StyledSignup>
+};
+
+export default Signup;
