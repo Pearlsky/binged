fae7a2c663ce4de8759a16749dc9e2dbcc45025f
Pearl Akpan <pearlakpan115@gmail.com>
2022-12-06 17:10:17 +0100

    refactor: created route path variables

---
+++ src/constants/routes.js
@@ -0,0 +1,6 @@
+export const LOGIN = "/login";
+export const SIGN_UP = "/signup";
+export const HOME = "/home";
+export const MOVIES = "/movies";
+export const TV_SERIES = "/tv-series";
+export const BOOKMARKS = "/bookmarked";
