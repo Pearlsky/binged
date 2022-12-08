46392bc97af396af49e11f1d24f194abee030ee1
Pearl Akpan <pearlakpan115@gmail.com>
2022-12-08 16:18:22 +0100

    refactor: del login and sign up page styles + unify same in auth page styles

--- src/views/Login/Login.styles.js
+++
@@ -1,9 +0,0 @@
-import styled from "styled-components";
-
-const StyledLogin = styled.main`
-    display: flex;
-    flex-direction: column;
-    height: 100vh;
-`;
-
-export default StyledLogin;
--- src/views/Signup/Signup.styles.js
+++
@@ -1,9 +0,0 @@
-import styled from "styled-components";
-
-const StyledSignup = styled.main`
-    display: flex;
-    flex-direction: column;
-    height: 100vh;
-`;
-
-export default StyledSignup;
