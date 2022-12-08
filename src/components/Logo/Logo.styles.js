f4c44aac6aa1b0be48d7e9818a17c487ebafd404
Pearl Akpan <pearlakpan115@gmail.com>
2022-12-08 15:56:29 +0100

    styles: write styles for reusable logo component

---
+++ src/components/Logo/Logo.styles.js
@@ -0,0 +1,14 @@
+import styled from "styled-components";
+
+const StyledLogo = styled.div`
+    width: 40px;
+    height: 40px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+
+    img {
+        max-width: 100%;
+    }
+`;
+export default StyledLogo;
