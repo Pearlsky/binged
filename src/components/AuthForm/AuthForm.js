2e333f75f19d18e1b13d4b79d2b49afcdb230fe3
Pearl Akpan <pearlakpan115@gmail.com>
2022-12-08 16:11:42 +0100

    refactor: add conditional rendering for some elements and add google auth input

--- src/components/AuthForm/AuthForm.js
+++ src/components/AuthForm/AuthForm.js
@@ -2,20 +2,33 @@ import { Link } from "react-router-dom";
 
 import * as ROUTES from "../../constants/routes";
 
-import Button from "../Button/Button";
+import { TextButton, OutlinedButton } from "../Button/Button";
 import TextField from "../TextField/TextField";
+import StyledAuthForm from "./AuthForm.styles";
+
+import googleIcon from "../../assets/Google.svg";
 
 const AuthForm = ({ heading }) => {
   return (
-    <form>
+    <StyledAuthForm>
       <h1>{heading}</h1>
       <TextField type="email" placeholder="Email address" />
       <TextField type="password" placeholder="Password" />
-      <Button
+      {heading === "login" ? (
+        ""
+      ) : (
+        <TextField type="password" placeholder="Repeat Password" />
+      )}
+      <TextButton
         type="submit"
         text={
           heading === "login" ? "Login to your account" : "Create an account"
         }
+      />
+      <OutlinedButton
+        icon={googleIcon}
+        text={heading === "login" ? "Login with Google" : "Sign up with Google"}
+        type="submmit"
       />
       {heading === "login" ? (
         <p>
@@ -26,7 +39,7 @@ const AuthForm = ({ heading }) => {
           Already have an account? <Link to={ROUTES.LOGIN}>Login</Link>
         </p>
       )}
-    </form>
+    </StyledAuthForm>
   );
 };
 
