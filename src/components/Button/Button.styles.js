c598d25eeec170ee739ca442349f9e17490fc1c8
Pearl Akpan <pearlakpan115@gmail.com>
2022-12-08 16:03:34 +0100

    styles: extend button styles for outlined btn component

--- src/components/Button/Button.styles.js
+++ src/components/Button/Button.styles.js
@@ -2,12 +2,35 @@ import styled from "styled-components";
 import { colors as color } from "../../utils/variables";
 
 const StyledButton = styled.button`
+  font-family: inherit;
+  font-weight: inherit;
   background-color: ${color.redprimary};
   border: none;
-  border-radius: 10px;
+  border-radius: 5px;
   color: ${color.white};
-  padding: 1.5em;
+  padding: 1.1em;
+  display: block;
+  margin-block: 2.5em 1em;
   width: 100%;
+
+  :hover {
+    background-color: ${color.white};
+    color: ${color.darkblue};
+  }
+
 `;
 
-export default StyledButton;
+const StyledOutlinedButton = styled(StyledButton)`
+  background-color: transparent;
+  border: solid 1px ${color.grayishblue};
+  display: flex;
+  margin-block: 0 1.5em;
+  justify-content: center;
+  align-items: center;
+
+  div {
+    margin-right: .7em;
+  }
+`;
+
+export { StyledButton, StyledOutlinedButton };
