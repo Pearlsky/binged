5388fe4643a111ee1af477a333b222f525c26efb
Pearl Akpan <pearlakpan115@gmail.com>
2022-12-08 16:12:17 +0100

    styles: refactor styles for auth form buttons

---
+++ src/components/AuthForm/AuthForm.styles.js
@@ -0,0 +1,30 @@
+import styled from "styled-components";
+import { colors as color, weights as weight } from "../../utils/variables";
+
+const StyledAuthForm = styled.form`
+  background-color: ${color.semidarkblue};
+  border-radius: 10px;
+  font-weight: ${weight.light};
+  font-family: "Outfit", sans-serif;
+  margin-top: 4em;
+  margin-inline: 2em;
+  width: 90%;
+  max-width: 400px;
+  padding: 1em 2em 1.5em;
+
+  a {
+    color: ${color.redprimary};
+    margin-left: 0.5em;
+  }
+
+  button {
+    font-size: 1rem;
+  }
+
+  p {
+    text-align: center;
+    margin: none;
+  }
+`;
+
+export default StyledAuthForm;
