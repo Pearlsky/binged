d2f0bcb2f96d762736d72cffb3d10f41aa052681
Pearl Akpan <pearlakpan115@gmail.com>
2022-12-12 02:34:31 +0100

    chore: remove onClick event prop from auth submit button

--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -1,16 +1,16 @@
 import Icon from "../Icon/Icon";
 import { StyledButton, StyledOutlinedButton } from "./Button.styles";
 
-const TextButton = ({ text, type = "", onClick}) => {
-  return <StyledButton type={type} onClick={onClick}>{text}</StyledButton>;
+const TextButton = ({ text, type = "" }) => {
+  return <StyledButton type={type}>{text}</StyledButton>;
 };
 
-const OutlinedButton = ({ text = "", icon, type = "", onClick }) => {
+const OutlinedButton = ({ text = "", icon, type = "" }) => {
   return (
-    <StyledOutlinedButton type={type} onClick={onClick}>
+    <StyledOutlinedButton type={type}>
       <Icon icon={icon} />
       {text}
-    </StyledOutlinedButton> 
+    </StyledOutlinedButton>
   );
 };
 
