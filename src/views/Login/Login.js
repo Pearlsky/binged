b6d91b10fd0506d75db28d206a7b87f9931ae07f
Pearl Akpan <pearlakpan115@gmail.com>
2022-12-17 21:08:09 +0100

    refactor: move alert component to auth page component

--- src/views/Login/Login.js
+++ src/views/Login/Login.js
@@ -1,10 +1,8 @@
-import Alert from "../../components/Alert/Alert";
 import AuthPage from "../../components/AuthPage/AuthPage";
 
 const Login = () => {
   return (
     <>
-      <Alert type="success" />
       <AuthPage heading="login" />
     </>
   );
