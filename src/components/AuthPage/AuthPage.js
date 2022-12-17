0443201f5171596a25a99d99fcd67567ccdb8da0
Pearl Akpan <pearlakpan115@gmail.com>
2022-12-17 21:11:22 +0100

    refactor: add alert component and manage success, err and err message states for deps components

--- src/components/AuthPage/AuthPage.js
+++ src/components/AuthPage/AuthPage.js
@@ -1,12 +1,18 @@
+import { useState } from "react";
+import Alert from "../Alert/Alert";
+
 import AuthForm from "../AuthForm/AuthForm";
 import Logo from "../Logo/Logo";
 import StyledAuthPage from "./AuthPage.styles";
 
 const AuthPage = ({ heading }) => {
+  const [status, setStatus] = useState("");
+  const [errMessage, setErrMessage] =  useState("");
   return (
     <StyledAuthPage heading={heading}>
+      <Alert type={status} setStatus={setStatus} errMessage={errMessage} />
       <Logo />
-      <AuthForm heading={heading} />
+      <AuthForm heading={heading} setStatus={setStatus} setErrMessage={setErrMessage}/>
     </StyledAuthPage>
   );
 };
