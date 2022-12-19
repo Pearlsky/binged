546a3aac77d355ba228c12339bbf30c019b81201
Pearl Akpan <pearlakpan115@gmail.com>
2022-12-19 20:38:08 +0100

    refactor: write conditional routes depending on auth state

--- src/components/App.js
+++ src/components/App.js
@@ -1,24 +1,31 @@
-import {Routes, Route} from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
+import { useContext } from "react";
+import { AuthContext } from "../services/firebase/auth";
 
-import GlobalStyles from '../utils/Global.styles';
+import GlobalStyles from "../utils/Global.styles";
 import * as ROUTES from "../constants/routes";
 
 import Login from "../views/Login/Login";
 import Signup from "../views/Signup/Signup";
 
 function App() {
+  const { user } = useContext(AuthContext);
   return (
     <>
-    <GlobalStyles/>
+      <GlobalStyles />
 
-    <Routes>
-      <Route path={ROUTES.SIGN_UP} element={<Signup/>}/>
-      <Route path={ROUTES.LOGIN} element={<Login/>}/>
-      {/* <Route path={ROUTES.HOME}/>
-      <Route path={ROUTES.MOVIES}/>
-      <Route path={ROUTES.TV_SERIES}/>
-      <Route path={ROUTES.BOOKMARKS}/> */}
-    </Routes>
+      <Routes>
+        <Route path={ROUTES.SIGN_UP} element={<Signup />} />
+        <Route path={ROUTES.LOGIN} element={<Login />} />
+        {user !== "" && (
+          <>
+            <Route path={ROUTES.HOME} />
+            <Route path={ROUTES.MOVIES} />
+            <Route path={ROUTES.TV_SERIES} />
+            <Route path={ROUTES.BOOKMARKS} />
+          </>
+        )}
+      </Routes>
     </>
   );
 }
