372718d3c60bf558ca122114654494cb972939d4
Pearl Akpan <pearlakpan115@gmail.com>
2022-12-20 04:12:13 +0100

    refactor: lift status alert-related state up

--- src/components/App.js
+++ src/components/App.js
@@ -1,22 +1,42 @@
 import { Routes, Route } from "react-router-dom";
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import { AuthContext } from "../services/firebase/auth";
 
 import GlobalStyles from "../utils/Global.styles";
 import * as ROUTES from "../constants/routes";
 
-import Login from "../views/Login/Login";
-import Signup from "../views/Signup/Signup";
+import Alert from "./Alert/Alert";
+import AuthPage from "./AuthPage/AuthPage";
 
 function App() {
   const { user } = useContext(AuthContext);
+  const [status, setStatus] = useState("");
+  const [errMessage, setErrMessage] = useState("");
   return (
     <>
       <GlobalStyles />
-
+      <Alert type={status} errMessage={errMessage}/>
       <Routes>
-        <Route path={ROUTES.SIGN_UP} element={<Signup />} />
-        <Route path={ROUTES.LOGIN} element={<Login />} />
+        <Route
+          path={ROUTES.SIGN_UP}
+          element={
+            <AuthPage
+              heading="signup"
+              setStatus={setStatus}
+              setErrMessage={setErrMessage}
+            />
+          }
+        />
+        <Route
+          path={ROUTES.LOGIN}
+          element={
+            <AuthPage
+              heading="login"
+              setStatus={setStatus}
+              setErrMessage={setErrMessage}
+            />
+          }
+        />
         {user !== "" && (
           <>
             <Route path={ROUTES.HOME} />
