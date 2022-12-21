de6d1c53d6b27c030bf0a853a8585738a25130e5
Pearl Akpan <pearlakpan115@gmail.com>
2022-12-21 22:52:34 +0100

    refactor (a11y): define alert role on container comp and aria-live attr_

--- src/components/Alert/Alert.js
+++ src/components/Alert/Alert.js
@@ -5,20 +5,24 @@ import StyledAlert from "./Alert.styles";
 const Alert = ({ type, errMessage }) => {
   return (
     <StyledAlert type={type} errMessage={errMessage}>
-      <div>
-        <img alt="" src={type === "success" ? successIcon : errorIcon} />
-        <p>
-          {type === "success" ? (
-            <span>
-              <strong>Success! </strong>User authenticated.
-            </span>
-          ) : (
-            <span>
-              <strong>Ooops! </strong>
-              {errMessage}.
-            </span>
-          )}
-        </p>
+      <div role="alert" aria-live="polite">
+        {type && (
+          <>
+            <img alt="" src={type === "success" ? successIcon : errorIcon} />
+            <p>
+              {type === "success" ? (
+                <span>
+                  <strong>Success! </strong>User authenticated.
+                </span>
+              ) : (
+                <span>
+                  <strong>Ooops! </strong>
+                  {errMessage}.
+                </span>
+              )}
+            </p>
+          </>
+        )}
       </div>
     </StyledAlert>
   );
