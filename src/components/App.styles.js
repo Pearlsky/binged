d752a7959bf546649172792d75566489ebedf8bd
Pearl Akpan <pearlakpan115@gmail.com>
2022-12-21 22:51:29 +0100

    style: write app container specific styles

---
+++ src/components/App.styles.js
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+const StyledApp = styled.div`
+  width: 100%;
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  max-width: 1800px;
+`;
+
+export default StyledApp;
