22aa1014c93222b774e635018f1891009f172e8d
Pearl Akpan <pearlakpan115@gmail.com>
2022-12-24 10:27:03 +0100

    styles: extend avatar styles for pc views

--- src/components/Avatar/Avatar.styles.js
+++ src/components/Avatar/Avatar.styles.js
@@ -1,17 +1,24 @@
 import styled from "styled-components";
 import { colors as color } from "../../utils/variables";
+import { breakpoints as breakpoint } from "../../utils/variables";
 
 const StyledAvatar = styled.article`
   width: 30px;
   height: 30px;
   border-radius: 50%;
   background-color: ${({ avatarImg }) =>
-    avatarImg ? "transparent" : "yellow"};
-  border: solid 2px ${color.white};
+    avatarImg ? "transparent" : color.grayishblue};
+  border: solid 1px ${color.white};
 
   img {
     max-width: 100%;
     border-radius: 50%;
+  }
+
+  @media (${breakpoint.laptop}) {
+    margin-top: auto;
+    width: 40px;
+    height: 40px;
   }
 `;
 
