7a811dbc20cf8042bf2dead492c728cd9a788b47
Pearl Akpan <pearlakpan115@gmail.com>
2022-12-25 22:36:59 +0100

    chore: clean up test component

--- src/views/Home/Home.js
+++ src/views/Home/Home.js
@@ -1,11 +1,15 @@
+import { PlayButton } from "../../components/MovieListing/MovieListing";
 import StyledHomeView from "./Home.styles";
 
 const Home = () => {
   return (
-    <StyledHomeView
-      role="region"
-      aria-label="Home Tab Content"
-    ></StyledHomeView>
+    <>
+      <StyledHomeView
+        role="region"
+        aria-label="Home Tab Content"
+      ></StyledHomeView>
+      <PlayButton/>
+    </>
   );
 };
 
