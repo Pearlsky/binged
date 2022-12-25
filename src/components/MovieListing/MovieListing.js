738c4491eeaf9760e05664a1964a890af061f145
Pearl Akpan <pearlakpan115@gmail.com>
2022-12-25 22:31:49 +0100

    chore: write bookmark button and play button components

---
+++ src/components/MovieListing/MovieListing.js
@@ -0,0 +1,25 @@
+import { useState } from "react";
+import { StyledBookmarkButton, StyledPlayButton } from "./MovieListing.styles";
+
+export const BookmarkButton = () => {
+  const [isBookmarked, setIsBookmarked] = useState(false);
+  const bookmarkHandler = () => {
+    setIsBookmarked(!isBookmarked);
+  };
+
+  return (
+    <StyledBookmarkButton
+      isBookmarked={isBookmarked}
+      onClick={() => bookmarkHandler()}
+    />
+  );
+};
+
+export const PlayButton = () => {
+    return (
+        <StyledPlayButton aria-labelledby="playBtn ">
+            <svg width="30" height="30" xmlns="http://www.w3.org/2000/svg"><path d="M15 0C6.713 0 0 6.713 0 15c0 8.288 6.713 15 15 15 8.288 0 15-6.712 15-15 0-8.287-6.712-15-15-15Zm-3 21V8l9 6.5-9 6.5Z" fill="#FFF"/></svg>
+            <span id="playBtn">Play</span>
+        </StyledPlayButton>
+    )
+}
