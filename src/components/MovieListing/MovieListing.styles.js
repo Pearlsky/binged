252995160aad360ccbd4cd08b969d6d66f09c24b
Pearl Akpan <pearlakpan115@gmail.com>
2022-12-26 16:09:58 +0100

    styles: write movie listing component styles

--- src/components/MovieListing/MovieListing.styles.js
+++ src/components/MovieListing/MovieListing.styles.js
@@ -1,21 +1,49 @@
 import styled from "styled-components";
 import { colors as color, weights as weight } from "../../utils/variables";
 
-import bookmarkIconFull from "../../assets/icon-bookmark-full.svg";
-import bookmarkIconEmpty from "../../assets/icon-bookmark-empty.svg";
 import movieCategoryIcon from "../../assets/icon-category-movie.svg";
 import tvseriesCategoryIcon from "../../assets/icon-category-tv.svg"
 
-const StyledMovieListing = styled.article``;
+const StyledMovieListing = styled.article`
+    width: 270px;
+    height: 250px;
+    display: flex;
+    flex-direction: column;
+`;
 
-export const StyledMovieDetails = styled.div`
+export const StyledMovieThumbnail = styled.div`
+    border-radius: 10px;
+    flex-basis: 70%;
+    position: relative;
+    background-color: ${color.grayishblue};
+    display: flex;
+    justify-content: center;
+    align-items: center;
+
+    :hover {
+        cursor: pointer;
+        button {
+            display: flex;
+        }
+    }
+`;
+
+export const StyledMovieTitle = styled.h3`
+    margin-block: 0;
+    font-size: 1.16rem;
+    font-weight: ${weight.medium};
+`;
+
+export const StyledMovieSubDetails = styled.div`
     display: flex;
     align-items: center;
     height: fit-content;
+    margin-block: .7em .3em;
+    color: rgba(255, 255, 255, 0.8);
     > span {
         display: block;
-        height: 2px;
-        width: 2px;
+        height: 3px;
+        width: 3px;
         border-radius: 50%;
         background-color: ${color.gray};
         margin-inline: .5em;
@@ -32,42 +60,6 @@ export const StyledMovieDetails = styled.div`
             width: 1em;
         }
     }
-`;
-
-export const StyledBookmarkButton = styled.button`
-  background: no-repeat url(${({ isBookmarked }) => isBookmarked ? bookmarkIconFull : bookmarkIconEmpty});
-  background-color: rgba(16, 20, 30, 0.6);
-  border: none;
-  background-position: center;
-  background-size: 15px;
-  width: 3em;
-  height: 3em;
-  text-indent: 4em;
-  white-space: nowrap;
-  overflow: hidden;
-  padding: 1em;
-  border-radius: 50%;
-`;
-
-export const StyledPlayButton = styled.button`
-  font-size: 1.15rem;
-  height: fit-content;
-  background-color: rgba(255, 255, 255, 0.2);
-  border-radius: 30px;
-  border: none;
-  color: ${color.white};
-  padding-block: 0.7em;
-  padding-inline: 0.7em;
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-  font-weight: ${weight.medium};
-  font-family: "Outfit", sans-serif;
-
-  svg,
-  span {
-    margin-right: 1.1em;
-  }
 `;
 
 export default StyledMovieListing;
