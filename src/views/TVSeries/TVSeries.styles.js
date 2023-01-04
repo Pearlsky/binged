6d998c6c8762573552bd890c61df864ff37ca3f3
Pearl Akpan <pearlakpan115@gmail.com>
2023-01-04 23:47:51 +0100

    styles: write styles for TV series view

--- src/views/TVSeries/TVSeries.styles.js
+++ src/views/TVSeries/TVSeries.styles.js
@@ -1,5 +1,13 @@
 import styled from "styled-components";
+import { breakpoints as breakpoint } from "../../utils/variables";
 
-const StyledTVSeriesView = styled.section` `;
+const StyledTVSeriesView = styled.section`
+  width: 100%;
+  padding-inline: 1em;
+  padding-block: 1em 1.5em;
+  @media (${breakpoint.laptop}) {
+    margin: 2em;
+  }
+`;
 
 export default StyledTVSeriesView;
