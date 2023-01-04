83a0cdc1fe8f8c4a0395b60cc762bab072118e0b
Pearl Akpan <pearlakpan115@gmail.com>
2023-01-04 23:47:28 +0100

    chore: write TV series view using user extracted data

--- src/views/TVSeries/TVSeries.js
+++ src/views/TVSeries/TVSeries.js
@@ -1,7 +1,31 @@
+import { get, ref } from "firebase/database";
+import { useContext, useEffect, useState } from "react";
+import Searchbar from "../../components/SearchBar/Searchbar";
+import { AuthContext } from "../../services/firebase/auth";
+import { DbContext } from "../../services/firebase/db";
+import { RegularSection } from "../Home/Home";
 import StyledTVSeriesView from "./TVSeries.styles";
 
 const TVSeries = () => {
-  return (<StyledTVSeriesView role="region" aria-label="TV Series tab content"></StyledTVSeriesView>) ;
+  const [tvSeriesListings, setTVSeriesListings] = useState();
+  const db = useContext(DbContext);
+  const { user } = useContext(AuthContext);
+
+  useEffect(() => {
+    const listingsRef = ref(db, `users/${user?.uid}/userListing`);
+    get(listingsRef).then((snapshot) => {
+      const userListings = snapshot
+        .val()
+        ?.filter((item) => item.category === "TV Series");
+      setTVSeriesListings(userListings);
+    });
+  }, [db, user?.uid]);
+  return (
+    <StyledTVSeriesView role="region" aria-label="TV Series tab content">
+      <Searchbar keyword="TV series" />
+      <RegularSection heading="TV Series" listings={tvSeriesListings} />
+    </StyledTVSeriesView>
+  );
 };
 
 export default TVSeries;
