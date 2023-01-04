2c29351decda7126534b7754754566f0ac98b9d4
Pearl Akpan <pearlakpan115@gmail.com>
2023-01-04 23:43:41 +0100

    styles: write styles for bookmark view

--- src/views/Bookmarks/Bookmarks.styles.js
+++ src/views/Bookmarks/Bookmarks.styles.js
@@ -1,7 +1,13 @@
 import styled from "styled-components";
+import { breakpoints as breakpoint } from "../../utils/variables";
 
 const StyledBookmarksView = styled.section`
-
+  width: 100%;
+  padding-inline: 1em;
+  padding-block: 1em 1.5em;
+  @media (${breakpoint.laptop}) {
+    margin: 2em;
+  }
 `;
 
 export default StyledBookmarksView;
