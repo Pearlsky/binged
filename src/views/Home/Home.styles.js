2df6242ee8526f42df37ace1f21306f82a77c12e
Pearl Akpan <pearlakpan115@gmail.com>
2023-01-04 23:45:22 +0100

    styles: write layout-grid styles for regular lisitings section

--- src/views/Home/Home.styles.js
+++ src/views/Home/Home.styles.js
@@ -1,7 +1,51 @@
 import styled from "styled-components";
+import {
+  weights as weight,
+  breakpoints as breakpoint,
+} from "../../utils/variables";
 
 const StyledHomeView = styled.section`
-    
+  width: 100%;
+  padding-inline: 1em;
+  padding-block: 1em 1.5em;
+  @media (${breakpoint.laptop}) {
+    margin: 2em;
+  }
+`;
+
+export const StyledRegularSection = styled.section`
+  width: 100%;
+  h2 {
+    font-weight: ${weight.light};
+    font-size: 1.5rem;
+    margin-block: 1.2em;
+    @media (${breakpoint.laptop}) {
+        font-size: 1.6rem;
+    }
+  }
+`;
+
+export const StyledListingGrid = styled.section`
+  width: 100%;
+  display: grid;
+  grid-template-columns: repeat(2, 1fr);
+  grid-auto-rows: 12rem;
+  gap: 1.3em;
+
+  @media (min-width: 38em) {
+    grid-template-columns: repeat(3, 1fr);
+  }
+
+  @media (${breakpoint.tablet}) {
+    grid-auto-rows: 14rem;
+    gap: 1.5em;
+  }
+
+  @media (${breakpoint.laptop}) {
+    grid-template-columns: repeat(4, 1fr);
+    grid-auto-rows: 17rem;
+    gap: 2em;
+  }
 `;
 
 export default StyledHomeView;
