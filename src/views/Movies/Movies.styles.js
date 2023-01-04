2a114031a1926c10e94823d43ff8276013f67e0f
Pearl Akpan <pearlakpan115@gmail.com>
2023-01-04 23:46:33 +0100

    styles: write responsive movie view styles

--- src/views/Movies/Movies.styles.js
+++ src/views/Movies/Movies.styles.js
@@ -1,5 +1,13 @@
 import styled from "styled-components";
+import { breakpoints as breakpoint } from "../../utils/variables";
 
-const StyledMoviesView = styled.section` `;
+const StyledMoviesView = styled.section`
+  width: 100%;
+  padding-inline: 1em;
+  padding-block: 1em 1.5em;
+  @media (${breakpoint.laptop}) {
+    margin: 2em;
+  }
+`;
 
 export default StyledMoviesView;
