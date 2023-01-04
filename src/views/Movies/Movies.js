aefa27a35ba8c9c76581745ab233710bc933dfa9
Pearl Akpan <pearlakpan115@gmail.com>
2023-01-04 23:46:04 +0100

    chore: sort user db listings to form movie listings

--- src/views/Movies/Movies.js
+++ src/views/Movies/Movies.js
@@ -1,7 +1,32 @@
+import { get, ref } from "firebase/database";
+import { useContext, useEffect, useState } from "react";
+import Searchbar from "../../components/SearchBar/Searchbar";
+import { AuthContext } from "../../services/firebase/auth";
+import { DbContext } from "../../services/firebase/db";
+import { RegularSection } from "../Home/Home";
 import StyledMoviesView from "./Movies.styles";
 
 const Movies = () => {
-  return (<StyledMoviesView role="region" aria-label="Movies tab content"></StyledMoviesView>);
+  const [moviesListings, setMoviesListings] = useState();
+  const db = useContext(DbContext);
+  const { user } = useContext(AuthContext);
+
+  useEffect(() => {
+    const listingsRef = ref(db, `users/${user?.uid}/userListing`);
+    get(listingsRef).then((snapshot) => {
+      const userListings = snapshot
+        .val()
+        ?.filter((item) => item.category === "Movie");
+
+      setMoviesListings(userListings);
+    });
+  }, [db, user?.uid]);
+  return (
+    <StyledMoviesView role="region" aria-label="Movies tab content">
+      <Searchbar keyword="movies" />
+      <RegularSection heading="Movies" listings={moviesListings} />
+    </StyledMoviesView>
+  );
 };
 
 export default Movies;
