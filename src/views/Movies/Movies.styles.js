915ce2ea59ad65ece2543cc7bfed433527411265
pearlsky <pearlakpan115@gmail.com>
2023-01-13 21:12:16 +0100

    refactor: control content horizontal overflow by setting scroll

--- src/views/Movies/Movies.styles.js
+++ src/views/Movies/Movies.styles.js
@@ -3,6 +3,7 @@ import { breakpoints as breakpoint } from "../../utils/variables";
 
 const StyledMoviesView = styled.section`
   width: 100%;
+  overflow: hidden scroll;
   padding-inline: 1em;
   padding-block: 1em 1.5em;
   @media (${breakpoint.laptop}) {
