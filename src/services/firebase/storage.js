260e2ef425edae56e2dd8f42ad9886dd793ecccc
pearlsky <pearlakpan115@gmail.com>
2023-01-15 23:42:26 +0100

    refactor: rm unneded storage context

--- src/services/firebase/storage.js
+++ src/services/firebase/storage.js
@@ -1,14 +1,5 @@
 import firebase from "./firebase";
 import { getStorage } from "firebase/storage";
-import { createContext } from "react";
 
 const storage = getStorage(firebase);
-export const StorageContext = createContext();
-
-export const StorageProvider = ({ children }) => {
-  return (
-    <StorageContext.Provider value={storage}>
-      {children}
-    </StorageContext.Provider>
-  );
-};
+export default storage;
