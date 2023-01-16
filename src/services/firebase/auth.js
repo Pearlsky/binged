bd7c8aaaaae3adf01c82e426bb8f453010cf2a70
pearlsky <pearlakpan115@gmail.com>
2023-01-16 15:22:45 +0100

    refactor: aid session persistence by setting user init state to session storage val

--- src/services/firebase/auth.js
+++ src/services/firebase/auth.js
@@ -6,7 +6,9 @@ const auth = getAuth(firebase);
 const AuthContext = createContext();
 
 const AuthProvider = ({ children }) => {
-  const [user, setUser] = useState("");
+  const [user, setUser] = useState(() =>
+    JSON.parse(sessionStorage.getItem("authUser"))
+  );
   const isLoggedIn = !!user;
 
   return (
