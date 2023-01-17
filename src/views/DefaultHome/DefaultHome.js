2a928a54bd4134898881f320d5aa8aef1d4514f6
pearlsky <pearlakpan115@gmail.com>
2023-01-17 10:54:55 +0100

    refactor: write component UI for default home page

---
+++ src/views/DefaultHome/DefaultHome.js
@@ -0,0 +1,24 @@
+import { Link } from "react-router-dom";
+import { StyledDefaultHomePage } from "./DefaultHome.styles";
+import {
+  TextButtonPrimary,
+  OutlinedButton,
+} from "../../components/Button/Button";
+import * as ROUTES from "../../constants/routes";
+
+const DefaultHome = () => {
+  return (
+    <StyledDefaultHomePage>
+      <div>
+        <Link to={ROUTES.LOGIN} strict>
+          <TextButtonPrimary text="Login" type="button" />
+        </Link>
+        <Link to={ROUTES.SIGN_UP}>
+          <OutlinedButton text="Sign Up" type="button" />
+        </Link>
+      </div>
+    </StyledDefaultHomePage>
+  );
+};
+
+export default DefaultHome;
