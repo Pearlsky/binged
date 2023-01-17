c0eec7a575eaf6e2420e921d42e69e566f295a72
pearlsky <pearlakpan115@gmail.com>
2023-01-17 11:05:14 +0100

    chore: mv default home view styles to individual styled comps

--- src/components/App.styles.js
+++ src/components/App.styles.js
@@ -1,23 +1,9 @@
 import styled from "styled-components";
-import { breakpoints as breakpoint } from "../utils/variables";
-import homeBg from "../assets/homebg.png";
+import { breakpoints as breakpoint } from "../style-utils/variables";
 
 export const StyledAppContainer = styled.div`
   @media (${breakpoint.laptop}) {
     display: flex;
     height: ${({ isLoggedIn }) => (isLoggedIn ? "100%" : 0)};
   }
-`;
-
-export const StyledDefaultHomePage = styled.section`
-  background: url(${homeBg});
-  background-repeat: no-repeat;
-  background-size: cover;
-  background-position: center;
-  background-color: rgba(0, 0, 0, 0.6);
-  background-blend-mode: overlay;
-  width: 100%;
-  height: 100vh;
-  display: flex;
-  flex-direction: column;
 `;
