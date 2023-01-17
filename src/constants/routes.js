3b7e346dbd6d29837e51451d76998348d900f310
pearlsky <pearlakpan115@gmail.com>
2023-01-17 11:11:55 +0100

    refactor: add default home route

--- src/constants/routes.js
+++ src/constants/routes.js
@@ -1,6 +1,7 @@
 export const LOGIN = "/login";
 export const SIGN_UP = "/signup";
 export const HOME = "/home";
+export const DEFAULT_HOME = "/";
 export const MOVIES = "/movies";
 export const TV_SERIES = "/tv-series";
 export const BOOKMARKS = "/bookmarked";
