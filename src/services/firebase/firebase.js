066674107e6e4eefe4111214b49472daafa12c53
pearlsky <pearlakpan115@gmail.com>
2023-01-19 09:06:48 +0100

    fix: do a stupid thing to fix firebase auth/inavlid-api-key error

--- src/services/firebase/firebase.js
+++ src/services/firebase/firebase.js
@@ -1,13 +1,13 @@
 import { initializeApp } from "firebase/app";
 
 const appConfig = {
-    apiKey: process.env.REACT_APP_API_KEY,
-    authDomain: process.env.REACT_APP_AUTH_DOMAIN,
-    databaseURL: process.env.REACT_APP_DATABASE_URL,
-    projectId: process.env.REACT_APP_PROJECT_ID,
-    storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
-    messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
-    appId: process.env.REACT_APP_APP_ID
+  apiKey: "AIzaSyA_ceqtifwZfGJFWwG_JrITyF7rNVWEi3Q",
+  authDomain: "binged-bad03.firebaseapp.com",
+  databaseURL: "https://binged-bad03-default-rtdb.firebaseio.com",
+  projectId: "binged-bad03",
+  storageBucket: "binged-bad03.appspot.com",
+  messagingSenderId: "606210170840",
+  appId: "1:606210170840:web:a34f37b1f3c68051f79cad",
 };
 
 const firebaseApp = initializeApp(appConfig);
