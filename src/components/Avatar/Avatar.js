097b38603b190fc64b75b0032d80334af791fc74
pearlsky <pearlakpan115@gmail.com>
2023-01-27 13:53:13 +0100

    refactor: re-write avatar component as btn; implement profile menu

--- src/components/Avatar/Avatar.js
+++ src/components/Avatar/Avatar.js
@@ -1,10 +1,44 @@
-import StyledAvatar from "./Avatar.styles";
+import { useContext, useState } from "react";
+import { Link } from "react-router-dom";
+import { AuthContext } from "../../services/firebase/auth";
+import { signOut } from "firebase/auth";
+import { LogoutButton } from "../Button/Button";
+import StyledAvatar, { StyledAvatarMenu } from "./Avatar.styles";
 
 const Avatar = ({ avatarImg }) => {
+  const [menuIsOpen, setMenuIsOpen] = useState(false);
+
+  const avatarMenuToggle = (e) => {
+    setMenuIsOpen(!menuIsOpen);
+  };
+
   return (
-    <StyledAvatar avatarImg={avatarImg}>
+    <StyledAvatar avatarImg={avatarImg} onClick={avatarMenuToggle}>
       {avatarImg && <img src={avatarImg} alt="user avatar" />}
+      {menuIsOpen && <AvatarMenu />}
     </StyledAvatar>
+  );
+};
+
+export const AvatarMenu = () => {
+  const { auth } = useContext(AuthContext);
+  const logoutHandler = () => {
+    signOut(auth);
+  };
+  return (
+    <StyledAvatarMenu>
+      <ul>
+        <li key="item-1">
+          <Link to="/profile">View Profile</Link>
+        </li>
+        <li key="item-2">
+          <Link to="/settings">Settings</Link>
+        </li>
+        <li key="item-3">
+          <LogoutButton onClick={logoutHandler} />
+        </li>
+      </ul>
+    </StyledAvatarMenu>
   );
 };
 
