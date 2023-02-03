84b78725c6605175dd1ff63b735456c1c4a28a77
pearlsky <pearlakpan115@gmail.com>
2023-02-03 18:26:00 +0100

    chore: rename specific style-utils dir as generic utils dir

---
+++ src/utils/variables.js
@@ -0,0 +1,27 @@
+export const colors = {
+  redprimary: `rgb(252, 71, 71)`,
+  darkblue: `rgb(16, 20, 30)`,
+  grayishblue: `rgb(90, 105, 143)`,
+  semidarkblue: `rgb(22, 29, 47)`,
+  white: `rgb(255, 255, 255)`,
+  gray: `rgb(151, 151, 151)`,
+};
+
+export const weights = {
+  light: 300,
+  medium: 500,
+};
+
+export const breakpoints = {
+  // 1200px
+  desktop: "min-width: 83em",
+
+  // 950px
+  laptop: "min-width: 66em",
+
+  // 768px
+  tablet: "min-width: 53em",
+
+  // 400px
+  mobile: "min-width: 28em",
+};
